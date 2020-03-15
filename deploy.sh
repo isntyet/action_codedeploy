b55a03fabf893f625b7f96f7e66a475da27e6100
isntyet <isntyet@gmail.com>
2020-03-15 23:29:33 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,9 @@ CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 종료할것 없음."
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo "> kill -9 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
