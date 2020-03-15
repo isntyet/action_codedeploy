adf1a7e8fd9b8a98bba925083dfd681153e1f45d
isntyet <isntyet@gmail.com>
2020-03-15 21:37:31 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,9 @@ cd $REPOSITORY
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-
-sudo kill -9 `ps -ef | grep 'java -jar $JAR_PATH' | grep -v 'grep' | awk '{print $2}'`
+# 기존 app kill
+PS_CMD=`ps -ef | grep 'java -jar $JAR_PATH' | grep -v 'grep' | awk '{print $2}'`
+kill -9 ${PS_CMD}
 
 ps -ef | grep 'java -jar $JAR_PATH' | grep -v 'grep' | awk '{print $2}'
 
