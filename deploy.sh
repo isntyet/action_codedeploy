f7e6234762e1c1382e05851fba537f2b045a9b75
isntyet <isntyet@gmail.com>
2020-03-15 21:24:36 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,13 @@
 REPOSITORY=/opt/testapp
 cd $REPOSITORY
 
+
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+
+sudo kill -9 `ps -ef | grep 'java -jar $JAR_PATH' | grep -v 'grep' | awk '{print $2}'`
+
+ps -ef | grep 'java -jar $JAR_PATH' | grep -v 'grep' | awk '{print $2}'
 
 nohup java -jar $JAR_PATH >> /opt/run.log &
