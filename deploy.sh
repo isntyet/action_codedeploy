b1b8d6908c503cf8007d08d85b2812420b914d1b
isntyet <isntyet@gmail.com>
2020-03-15 22:24:25 +0900

    remove stop.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@
 REPOSITORY=/opt/testapp
 cd $REPOSITORY
 
+APP_NAME=action_codedeploy
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+# 기존 app kill
+sudo kill -9 `ps -ef | grep ${APP_NAME} | grep -v 'grep' | awk '{print $2}'`
 
 java -jar $JAR_PATH
--- stop.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-APP_NAME=action_codedeploy
-
-# 기존 app kill
-kill -9 `ps -ef | grep ${APP_NAME} | grep -v 'grep' | awk '{print $2}'`
-
